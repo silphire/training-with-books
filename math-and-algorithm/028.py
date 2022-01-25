13b07ba8e2656138ac5595b71975af3dd8966feb
Yuki Mitsui <silphire@gmail.com>
2022-01-26 06:24:55 +0900

    added 028 of math-and-algorithm

--- math-and-algorithm/028.py
+++ math-and-algorithm/028.py
@@ -1,9 +1,13 @@
-# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ab
+# https://atcoder.jp/contests/math-and-algorithm/tasks/dp_a
 
 n = int(input())
-dp = [0] * (n + 10)
-dp[0] = 1
+hh = list(map(int, input().split()))
+
+dp = [float('inf')] * n
+dp[0] = 0
 for i in range(n):
-    dp[i + 2] += dp[i]
-    dp[i + 1] += dp[i]
-print(dp[n])
+    if i + 1 < n:
+        dp[i + 1] = min(dp[i + 1], dp[i] + abs(hh[i + 1] - hh[i]))
+    if i + 2 < n:
+        dp[i + 2] = min(dp[i + 2], dp[i] + abs(hh[i + 2] - hh[i]))
+print(dp[n - 1])
