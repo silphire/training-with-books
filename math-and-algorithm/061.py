6954c5ac44453a063547ece944a2ae3cded57a0e
Yuki Mitsui <silphire@gmail.com>
2022-03-01 21:55:08 +0900

    removed useless comments of  061 of math-and-algorithm

--- math-and-algorithm/061.py
+++ math-and-algorithm/061.py
@@ -1,3 +1,5 @@
+# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ba
+
 n = int(input())
 x = 1
 while x <= n:
@@ -6,15 +8,3 @@ while x <= n:
         exit()
     x = x * 2 + 1
 print('First')
-
-# 1 Second
-# 2 F
-# 3 S
-# 4 F
-# 5 F
-# 6 F
-# 7 S
-# 8 F
-# 9 F
-# 10 F
-# 11 
