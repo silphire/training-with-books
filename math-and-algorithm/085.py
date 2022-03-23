2d1df260cace12478a4d79da91bb62ea5cb1adae
Yuki Mitsui <silphire@gmail.com>
2022-03-23 22:04:56 +0900

    updated 085 of math-and-algorithm

--- math-and-algorithm/085.py
+++ math-and-algorithm/085.py
@@ -1,9 +1,11 @@
+# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_bo
+
 n, x, y = map(int, input().split())
 for a in range(1, n + 1):
     for b in range(a, n + 1):
         for c in range(b, n + 1):
             d = x - a - b - c
-            if a * b * c * d == y:
+            if d <= n and a * b * c * d == y:
                 print('Yes')
                 exit()
 print('No')
