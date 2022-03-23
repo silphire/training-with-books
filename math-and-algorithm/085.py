8b3d3ef5a3e821d113feb64b592281696906575a
Yuki Mitsui <silphire@gmail.com>
2022-03-23 22:04:22 +0900

    added 085 of math-and-algorithm

---
+++ math-and-algorithm/085.py
@@ -0,0 +1,9 @@
+n, x, y = map(int, input().split())
+for a in range(1, n + 1):
+    for b in range(a, n + 1):
+        for c in range(b, n + 1):
+            d = x - a - b - c
+            if a * b * c * d == y:
+                print('Yes')
+                exit()
+print('No')
