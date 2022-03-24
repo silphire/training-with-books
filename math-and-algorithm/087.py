e8910ab087949cf95d650cb58d22f1e380e5a28b
Yuki Mitsui <silphire@gmail.com>
2022-03-24 13:08:53 +0900

    updated 087 of math-and-algorithm

--- math-and-algorithm/087.py
+++ math-and-algorithm/087.py
@@ -5,7 +5,7 @@ n = int(input())
 # {n(n+1)/2}^2
 # n^2(n+1)^2 / 4
 # n^2(n^2 + 2n + 1) / 4
-# n^4 + 2n^3 + n^2 / 4
+# (n^4 + 2n^3 + n^2) / 4
 ans = pow(n, 4, MOD) + 2 * pow(n, 3, MOD) + pow(n, 2, MOD)
 ans %= MOD
 ans *= pow(4, MOD - 2, MOD)
